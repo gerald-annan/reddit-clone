1928045f066f249008a58edad767363b3388db03
Gerald Annan <annang2@stanbic.com.gh>
2022-04-01 18:33:26 +0000

    import @mikro-orm, __prod__

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello there");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [],
+        dbName: 'liredit',
+        debug: !__prod__,
+        type: 'postgresql'
+    });
+}
+
+main();
