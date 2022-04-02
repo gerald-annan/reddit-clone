9311d3aa01eeecd92675f5856014b6baa66e8e77
Gerald Annan <annang2@stanbic.com.gh>
2022-04-02 21:27:06 +0000

    user entity migration

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 
 export default {
     allowGlobalContext: true,
@@ -9,7 +10,7 @@ export default {
         path: path.join(__dirname, "./migrations"),
         pattern: /^[\w-]+\d+\.[tj]s$/
     },
-    entities: [Post],
+    entities: [Post, User],
     dbName: 'lireddit',
     debug: !__prod__,
     type: 'postgresql'
