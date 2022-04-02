bf0baf0bd357b96db45000aaf8d730bde5fb3a5a
Gerald Annan <annang2@stanbic.com.gh>
2022-04-02 21:21:24 +0000

    create User entity

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import mikroOrmConfig from "./mikro-orm.config";
@@ -6,7 +7,6 @@ import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/posts";
-import "reflect-metadata";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig);
