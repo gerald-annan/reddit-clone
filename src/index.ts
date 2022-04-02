827de28348120c10c65e6a2cd8fcbe2c39714e58
Gerald Annan <annang2@stanbic.com.gh>
2022-04-02 14:30:55 +0000

    created graphql endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/posts";
+import "reflect-metadata";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig);
@@ -16,14 +17,15 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver],
+            resolvers: [HelloResolver, PostResolver],
             validate: false
-        })
+        }),
+        context: () => ({ em: orm.em })
     })
 
     apolloServer.start().then(_ => {
         apolloServer.applyMiddleware({ app });
-        
+
         app.listen({ port: 4000 }, () =>
             console.log('server started on http://localhost:4000')
         )
