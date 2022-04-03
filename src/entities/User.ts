8a67161947050bcb2467f32d7403010616b20d3c
Gerald Annan <annang2@stanbic.com.gh>
2022-04-03 20:17:27 +0000

    registration validation

---
+++ src/entities/User.ts
@@ -0,0 +1,32 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class User {
+    @Field()
+    @PrimaryKey()
+    id!: number;
+
+    @Field(() => String)
+    @Property({type: 'date'})
+    createdAt: Date = new Date();
+
+    @Field(() => String)
+    @Property({type: 'date', onUpdate: () => new Date()})
+    updatedAt: Date = new Date();
+
+    @Field()
+    @Property({type: 'text', unique: true})
+    username!: string;
+
+    @Property({type: 'text', unique: true})
+    password!: string;
+
+    constructor(username: string, password: string) {
+        this.username = username;
+        this.password = password;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+}
