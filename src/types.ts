94dcc89f0ee913aa10db265807eb87748ff13e54
Gerald Annan <annang2@stanbic.com.gh>
2022-04-07 23:10:44 +0000

    updated @types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,7 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
 
 export type MyContext = {
-    em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
+    em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+    req: Request;
+    res: Response;
 }
